b8bc4e839a9f1bbad1194996e571b6fe7e64b316
surajdombale <surajdombale@gmail.com>
2026-05-31 09:39:43 +0530

    reverse

--- src/linkedlist/ReversedLinked.java
+++ src/linkedlist/ReversedLinked.java
@@ -1,15 +1,15 @@
 package linkedlist;
 
+import util.ListNode;
 
 public class ReversedLinked {
 
     public static void run() {
         System.out.println("Reverse Linked List Result:");
         reverseLinkedList();
-    }   
+    }
 
-    
-        /*
+    /*
     =========================================================
     QUESTION 1: Reverse a Linked List
     =========================================================
@@ -25,5 +25,41 @@ public class ReversedLinked {
     4 -> 3 -> 2 -> 1 -> null
     */
 
+    public static void reverseLinkedList() {
+        ListNode head = new ListNode(1);
+        head.setNext(new ListNode(2));
+        head.getNext().setNext(new ListNode(3));
+        head.getNext().getNext().setNext(new ListNode(4));
 
+        System.out.println("Original Linked List:");
+        printList(head);
+
+        ListNode reversedHead = reverseList(head);
+
+        System.out.println("Reversed Linked List:");
+        printList(reversedHead);
+    }
+
+    public static ListNode reverseList(ListNode head) {
+        ListNode prev = null;
+        ListNode current = head;
+
+        while (current != null) {
+            ListNode next = current.getNext();
+            current.setNext(prev);
+            prev = current;
+            current = next;
+        }
+
+        return prev;
+    }
+
+    public static void printList(ListNode head) {
+        while (head != null) {
+            System.out.print(head.getVal() + " -> ");
+            head = head.getNext();
+        }
+        System.out.println("null");
+    }
+    
 }

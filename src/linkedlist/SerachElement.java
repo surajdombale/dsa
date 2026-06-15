d9d6c986b03f2cf5c98006b539ecf3c051477a78
surajdombale <surajdombale@gmail.com>
2026-06-15 13:18:37 +0530

    search element

---
+++ src/linkedlist/SerachElement.java
@@ -0,0 +1,26 @@
+package linkedlist;
+
+public class SerachElement {
+    public static void run() {
+        System.out.println("Search Element Result:");
+        searchElement();
+    }
+
+    public static void searchElement() {
+        // Create a linked list
+        ListNode head = new ListNode(1);
+        head.setNext(new ListNode(2));
+        head.getNext().setNext(new ListNode(3));
+        head.getNext().getNext().setNext(new ListNode(4));
+        head.getNext().getNext().getNext().setNext(new ListNode(5));
+
+        int target = 3;
+        boolean found = search(head, target);
+
+        if (found) {
+            System.out.println("Element " + target + " found in the linked list.");
+        } else {
+            System.out.println("Element " + target + " not found in the linked list.");
+        }
+    }
+}

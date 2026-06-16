3a9468bfc9072eb4b4f3d5e9c91fca1a67a85d96
surajdombale <surajdombale@gmail.com>
2026-06-16 22:18:58 +0530

    /

--- src/linkedlist/DeleteNode.java
+++ src/linkedlist/DeleteNode.java
@@ -6,7 +6,10 @@ public class DeleteNode {
 
     public static void run() {
         System.out.println("Delete Node Result:");
-        deleteNode();
+        ListNode head = new ListNode(1);
+        head.setNext(new ListNode(2));
+        head.getNext().setNext(new ListNode(3));
+        deleteNode(head.getNext().getNext());
     }
 
     
